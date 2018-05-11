7b275484e6005d1398495148cc7320aad9d880b3
Alex Zhang <alexgzhang92@gmail.com>
2018-05-11 22:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
   else {
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
+    return `Currently serving ${katzDeliLine[0]}.`;
   }
   katzDeliLine.shift();
 }
