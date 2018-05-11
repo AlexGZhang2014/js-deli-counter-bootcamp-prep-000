396c88809de28c9abc8d00b29163427179138d02
Alex Zhang <alexgzhang92@gmail.com>
2018-05-11 23:01:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
+  let array = [];
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
   else {
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
+    array.push(katzDeliLine[0]);
+    katzDeliLine.shift();
+    return `Currently serving ${array[0]}.`;
   }
-  katzDeliLine.shift();
 }
 
 function currentLine(katzDeliLine) {
